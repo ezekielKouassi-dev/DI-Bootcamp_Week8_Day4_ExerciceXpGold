0756b98884a53d2edb555c6a93be8f75b1b749c8
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2023-02-21 08:12:30 +0000

    first commit

---
+++ exerciceXpGoldWeek8Day4/src/main/java/di/atos/exercicexpgoldweek8day4/ExerciceXpGoldWeek8Day4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.atos.exercicexpgoldweek8day4;
+
+/**
+ *
+ * @author ezekielkouassi
+ */
+public class ExerciceXpGoldWeek8Day4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
